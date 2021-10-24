0cd8e8142037e776d4a8d0792fb4878f7041f94b
Lydia-09 <pasky0901@gmail.com>
2021-10-24 22:59:40 +0800

    modify restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,21 +1,52 @@
-const Restaurant = require('../restaurant')
-const seedData = require('./restaurant.json')
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const db = require('../../config/mongoose')
+const Restaurant = require('../restaurant')
+const seedData = require('./restaurant.json').results
+const User = require('../user')
+
+const SEED_USER = [
+  {
+    name: 'user1',
+    email: 'user1@example.com',
+    password: '12345678',
+    seedData_index: [0, 1, 2]
+  },
+  {
+    name: 'user2',
+    email: 'user2@example.com',
+    password: '12345678',
+    seedData_index: [3, 4, 5]
+  }
+]
 
 db.once('open', () => {
-  seedData.results.forEach((item) => {
-    Restaurant.create({
-      name: item.name,
-      name_en: item.name_en,
-      category: item.category,
-      image: item.image,
-      location: item.location,
-      phone: item.phone,
-      google_map: item.google_map,
-      rating: item.rating,
-      description: item.description
+  Promise.all(
+    Array.from(SEED_USER, seedUser => {
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(seedUser.password, salt))
+        .then(hash => User.create({
+          name: seedUser.name,
+          email: seedUser.email,
+          password: hash
+        }))
+        .then(user => {
+          const userId = user._id
+          const restaurant = []
+          Array.from(seedUser.seedData_index, index => {
+            seedData[index].userId = userId
+            restaurant.push(seedData[index])
+          })
+          return Restaurant.create(restaurant)
+        })
+    }))
+    .then(() => {
+      console.log('done')
+      process.exit()
     })
-  })
-
-  console.log('Seed data created done! Ctrl+C to Exit.')
+    .catch(err => console.log(err))
 })
