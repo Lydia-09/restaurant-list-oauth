23bbc296d4f63ca6a1baedde5377e8d9fc641ea1
Lydia-09 <pasky0901@gmail.com>
2021-10-24 20:00:08 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router()
 // 引入 home, restaurants 模組程式碼
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 // 將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/', home)
 router.use('/restaurants', restaurants)
+router.use('/users', users)
 // 匯出路由器
 module.exports = router
