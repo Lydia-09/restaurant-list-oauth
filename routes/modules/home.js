670d01c4c421d68a06d70da38b7484e2c4340ab6
Lydia-09 <pasky0901@gmail.com>
2021-10-24 22:14:23 +0800

    feat: add relations between User and Todo models

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,6 +5,7 @@ const Restaurant = require('../../models/restaurant')
 
 // 設定首頁路由
 router.get('/', (req, res) => {
+  const userId = req.user._id 
   // 取得排序選項
   const sortValueString = req.query.sort
   // 排序選項對應查詢
@@ -19,7 +20,7 @@ router.get('/', (req, res) => {
   // 提供boolean資訊給handlebars helper, 設定sort選項
   const sort = sortValueString ? { [sortValueString]: true } : { 'id': true }
 
-  Restaurant.find()
+  Restaurant.find({ userId })
   .lean()
   .sort(sortOption[sortValueString])
   .then(restaurants => res.render('index', { restaurants, sort }))
--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -8,15 +8,18 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-  return Restaurant.create(req.body)
+  const { name, name_en, category, image, location, phone, google_map, rating, description, website} = req.body
+  const userId = req.user._id
+  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description, website, userId })
     .then(() => res.redirect('/?sort=id'))
     .catch(error => console.log(error))
 })
 
 // 路由：瀏覽特定餐廳
 router.get('/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
     .lean()
     .then(restaurant => res.render('show', { restaurant }))
     .catch(error => console.log(error))
@@ -24,8 +27,9 @@ router.get('/:id', (req, res) => {
 
 // 路由：編輯特定餐廳
 router.get('/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
     .lean()
     .then((restaurant) => res.render('edit', { restaurant }))
     .catch(error => console.log(error))
@@ -33,16 +37,18 @@ router.get('/:id/edit', (req, res) => {
 
 // 路由：更新特定餐廳
 router.put('/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findByIdAndUpdate(id, { $set: req.body })
-    .then(()=> res.redirect(`/restaurants/${id}`))
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findByIdAndUpdate({ _id, userId }, { $set: req.body })
+    .then(()=> res.redirect(`/restaurants/${_id}`))
     .catch(error => console.log(error))
 })
 
 // 路由：刪除特定餐廳
 router.delete('/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
+   const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
     .then(restaurant => restaurant.remove())
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
